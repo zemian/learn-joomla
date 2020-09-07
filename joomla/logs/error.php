abffdf244cffac036a2d9c66dc56a880d56172f8
Zemian Deng <zemian.deng@oracle.com>
2020-09-07 09:58:16 -0400

    Update configuration

--- joomla/logs/error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2020-08-24 01:54:08 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2020-08-24T01:54:08+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
