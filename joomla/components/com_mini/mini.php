4fc077c6155ffb423de9a6eb7a78b4dae3801c48
Zemian Deng <zemian.deng@oracle.com>
2020-09-17 18:14:34 -0400

    Add log statement

--- joomla/components/com_mini/mini.php
+++ joomla/components/com_mini/mini.php
@@ -1,4 +1,8 @@
 <?php
 defined('_JEXEC') or die('Restricted Access');
 
+//JLog::add('my error message', JLog::ERROR);
+JLog::add('my info message', JLog::INFO);
+//JLog::add('my debug message', JLog::DEBUG);
+
 echo "Welcome to mini";
